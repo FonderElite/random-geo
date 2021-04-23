d240b2e436b21724a68471845c44e7d268dd7c43
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-23 11:43:34 +0000

    Update reverse_geo.py

--- reverse_geo.py
+++ reverse_geo.py
@@ -1,12 +1,8 @@
 import geopy
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
-latitude = input("Latitude: ")
-longitude  = input("Longitude: ")
-def reverse_geo_locate(lat,long): 
+def reverse_geo_locate(lat,long):
  locator = Nominatim(user_agent='Geo_Locator')
  coordinates = lat,long
  location = locator.reverse(coordinates)
- location.raw
  print(location)
-reverse_geo_locate(latitude,longitude)
