8059b806bddd779ed6a6a35442bde1e412bdb18a
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-23 10:01:48 +0000

    Update reverse_geo.py

--- reverse_geo.py
+++ reverse_geo.py
@@ -1,14 +1,12 @@
-import geopy,argparse
+import geopy
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
-parser = argparse.ArgumentParser(description="BruteForce Directory")
-parser.add_argument('-lat','--latitude',metavar='',help="Latitude Coordinate(y)")
-parser.add_argument('-long',"--longitude",metavar='',help="Longitude Coordinate(x)")
-args = parser.parse_args()
-def reverse_geo_locate(latgeo,longgeo):
- locator = Nominatim(user_agent=”myGeocoder”)
- coordinates = str(latgeo),str(longgeo)
+latitude = input("Latitude: ")
+longitude  = input("Longitude: ")
+def reverse_geo_locate(lat,long): 
+ locator = Nominatim(user_agent='Geo_Locator')
+ coordinates = lat,long
  location = locator.reverse(coordinates)
  location.raw
  print(location)
-reverse_geo_locate(args.latgeo,args.longgeo)
+reverse_geo_locate(latitude,longitude)
