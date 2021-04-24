13fc0ef0df8a58b059601f53768fb9fdd0dc514d
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-24 17:09:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 PIP=/usr/bin/pip
+DEP=/home/kali/requirements.txt
 sudo apt update
+modules=(geopy geocoder colorama requests)
 if [ -f '$PIP' ]
 then
 echo "pip is installed."
 else
 sudo apt install python3-pip
-sudo pip3 install requirements.txt
+for i in ${modules[@]}
+do
+sudo pip3 install $i
+done
 fi
